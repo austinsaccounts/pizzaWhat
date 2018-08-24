b0768a3db38decdb4e790ae8319ccbbb796cf09c
Austin Cummings <Austins.accounts@gmail.com>
2018-08-24 11:24:30 -0700

    use a prototype to write the pizza order in a readable form

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,29 @@ function Pizza(orderSize,orderSizePrice,orderToppings,toppingsPrice){
   this.toppings=orderToppings;
   this.toppingsPrice=toppingsPrice;
 }
+Pizza.prototype.assembler = function () {
+  var assembledPizza = "a " +this.size+ " " +this.toppings.join(" ")+ " pizza";
+  return assembledPizza;
+};
+
 //user interface
 $(document).ready(function() {
   $("#pizzaGate").submit(function(event) {
     event.preventDefault();
     var orderSize = $("input[type=radio][name=size]:checked").val();
     var orderSizePrice =$("input[type=radio][name=size]:checked").attr('sizePrice');
-    var toppingsPrice=[];
     var orderToppings=[];
     var orderCheese = $("input[type=radio][name=cheese]:checked").val()
     var orderCheesePrice =$("input[type=radio][name=cheese]:checked").attr('toppingsPrice');
     orderToppings = $("#pizzaGate  input:checkbox:checked").map(function(){
       return $(this).val();
       }).get();
-    toppingsPrice = $("#pizzaGate  input:checkbox:checked").map(function(){
+    var toppingsPrice = $("#pizzaGate  input:checkbox:checked").map(function(){
       return parseInt($(this).attr('toppingsPrice'));
       }).get();
-    orderToppings.push(orderCheese);
+    orderToppings.unshift(orderCheese);
+    toppingsPrice.unshift(parseInt(orderCheesePrice));
     var myPizza = new Pizza(orderSize,orderSizePrice,orderToppings,toppingsPrice);
-    console.log(myPizza);
+    $('#output').append('<p>'+myPizza.assembler()+'</p>');
   })
 })
