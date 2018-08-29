d1ce58a16a1f98dc668f1042b3397dabec15c65e
Austin Cummings <Austins.accounts@gmail.com>
2018-08-28 20:23:19 -0700

    refinished

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,10 @@ function getSum(total, num) {
     return total + num;
 }
 function Pizza(orderSize,orderSizePrice,orderToppings,toppingsPrice){
-  this.size=orderSize;
-  this.sizePrice=parseInt(orderSizePrice);
-  this.toppings=orderToppings;
-  this.toppingsPrice=toppingsPrice;
+  this.size = orderSize ;
+  this.sizePrice = parseInt(orderSizePrice);
+  this.toppings = orderToppings;
+  this.toppingsPrice = toppingsPrice;
 }
 Pizza.prototype.Assembler = function () {
   var assembledPizza = this.size + " " + this.toppings.join(", ");
@@ -34,7 +34,7 @@ $(document).ready(function() {
   $("#pizzaGate").submit(function(event) {
     event.preventDefault();
     var orderSize = $("input[type=radio][name=size]:checked").val();
-    var orderSizePrice =$("input[type=radio][name=size]:checked").attr('sizePrice');
+    var orderSizePrice = $("input[type=radio][name=size]:checked").attr('sizePrice');
     var orderToppings = [];
     var orderCheese = $("input[type=radio][name=cheese]:checked").val()
     var orderCheesePrice =  $("input[type=radio][name=cheese]:checked").attr('toppingsPrice');
